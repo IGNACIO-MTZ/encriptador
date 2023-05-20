e93dc17dd557e8146d0e845115c4c80219473568
Ignacio <ccnobe@gmail.com>
2023-05-20 01:16:18 -0600

    inicio de encriptador

---
+++ archivo.js
@@ -0,0 +1,84 @@
+var botonEncriptar = document.querySelector(".btn-encriptar");
+var botonDesencriptar = document.querySelector(".btn-desencriptar");
+var munheco = document.querySelector(".contenedor-munheco");
+var h3 = document.querySelector(".contenedor-h3");
+var parrafo = document.querySelector(".contenedor-p");
+var resultado = document.querySelector(".texto-resultado");
+var copiarResultado = document.querySelector(".btn-copiar");
+
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
+copiarResultado.onclick = copiar;
+
+function copiar() {
+  navigator.clipboard.writeText(resultado.innerHTML);
+  alert("texto copiado");
+}
+
+function encriptar() {
+  ocultarAdelante();
+  var area = recuperarTexto();
+  resultado.textContent = encriptarTexto(area);
+}
+function desencriptar() {
+  ocultarAdelante();
+  var area = recuperarTexto();
+  resultado.textContent = desencriptarTexto(area);
+}
+function recuperarTexto() {
+  var area = document.querySelector(".area");
+  return area.value;
+}
+
+function ocultarAdelante() {
+  munheco.classList.add("ocultar");
+  h3.classList.add("ocultar");
+  parrafo.classList.add("ocultar");
+}
+
+function encriptarTexto(mensaje) {
+  var texto = mensaje;
+  var textoFinal = "";
+  for (var i = 0; i < texto.length; i++) {
+    if (texto[i] == "a") {
+      textoFinal = textoFinal + "ai";
+    } else if (texto[i] == "e") {
+      textoFinal = textoFinal + "enter";
+    } else if (texto[i] == "i") {
+      textoFinal = textoFinal + "imes";
+    } else if (texto[i] == "o") {
+      textoFinal = textoFinal + "ober";
+    } else if (texto[i] == "u") {
+      textoFinal = textoFinal + "ufat";
+    } else {
+      textoFinal = textoFinal + texto[i];
+    }
+  }
+  return textoFinal;
+}
+function desencriptarTexto(mensaje) {
+  var texto = mensaje;
+  var textoFinal = "";
+
+  for (var i = 0; i < texto.length; i++) {
+    if (texto[i] == "a") {
+      textoFinal = textoFinal + "a";
+      i = i + 1;
+    } else if (texto[i] == "e") {
+      textoFinal = textoFinal + "e";
+      i = i + 4;
+    } else if (texto[i] == "i") {
+      textoFinal = textoFinal + "i";
+      i = i + 3;
+    } else if (texto[i] == "o") {
+      textoFinal = textoFinal + "o";
+      i = i + 3;
+    } else if (texto[i] == "u") {
+      textoFinal = textoFinal + "u";
+      i = i + 3;
+    } else {
+      textoFinal = textoFinal + texto[i];
+    }
+  }
+  return textoFinal;
+}
